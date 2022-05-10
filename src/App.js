fa6df5b8e76d29a340cdd0674502d1bc86cbe58f
Ann <cchouaa@connect.ust.hk>
2022-05-10 21:37:55 +0800

    image uploading to aws s3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { uploadFile } from 'react-s3';
+
 import './App.css';
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const config = {
+    bucketName: process.env.REACT_APP_AWS_S3_BUCKET,
+    region: process.env.REACT_APP_AWS_S3_REGION,
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_ACCESS_KEY_SECRET,
+}
+
+  const handleFileInput = (e) => {
+    setSelectedFile(e.target.files[0]);
+  }
+
+  const handleUpload = async (file) => {
+    uploadFile(file, config)
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>React S3 File Upload</div>
+      <input type="file" onChange={handleFileInput} />
+      <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
     </div>
   );
 }
