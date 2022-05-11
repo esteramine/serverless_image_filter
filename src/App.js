1b8fd0ced0482c41c37305fac234bf2584ac3095
Ann <cchouaa@connect.ust.hk>
2022-05-11 20:17:07 +0800

    drag and drop an image to s3 and store it with uuid as file name

--- src/App.js
+++ src/App.js
@@ -1,34 +1,90 @@
 import React, { useState } from 'react';
+import ReactImageUploading from 'react-images-uploading';
 import { uploadFile } from 'react-s3';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
+import Sidebar from './components/Sidebar';
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState([]);
+
+  const handleUpload = async (file) => {
+    uploadFile(file, config)
+      .then(data => setSelectedFile(data.location))
+      .catch(err => console.error(err))
+  }
+
+  const onChange = (imageList, addUpdateIndex) => {
+    const newFile = { ...imageList[0], name: uuidv4() }
+    setImage([newFile]);
+    handleUpload(imageList[0].file);
+  };
 
   const config = {
     bucketName: process.env.REACT_APP_AWS_S3_BUCKET,
     region: process.env.REACT_APP_AWS_S3_REGION,
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_ACCESS_KEY_SECRET,
-}
-
-  const handleFileInput = (e) => {
-    setSelectedFile(e.target.files[0]);
-  }
-
-  const handleUpload = async (file) => {
-    uploadFile(file, config)
-      .then(data => console.log(data))
-      .catch(err => console.error(err))
   }
 
   return (
-    <div className="App">
-      <div>React S3 File Upload</div>
-      <input type="file" onChange={handleFileInput} />
-      <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+    <div className="App" class='flex flex-row'>
+      <div class='w-2/12'>
+        <Sidebar />
+      </div>
+
+      <div class='flex flex-row w-10/12'>
+        <div class='w-6/12 border-r-2 border-gray-200 flex h-full justify-center items-center'>
+          <ReactImageUploading
+            value={image}
+            onChange={onChange}
+            dataURLKey="data_url"
+          >
+            {({
+              imageList,
+              onImageUpload,
+              onImageRemoveAll,
+              onImageUpdate,
+              onImageRemove,
+              isDragging,
+              dragProps,
+            }) => (
+              <div class='w-full'>
+                {image.length === 0 ? (
+                  <div class={'flex justify-center'}>
+                    <button
+                      class={(isDragging ? 'bg-gray-200' : 'bg-gray-100') + ' border-dashed border-4 border-gray-400 text-gray-600 font-bold w-10/12 py-24'}
+                      onClick={onImageUpload}
+                      {...dragProps}
+                    >
+                      Click or Drop An Image Here
+                    </button>
+                  </div>
+                ) : (
+                  <>
+                    <div class='flex flex-col h-full justify-center items-center px-4'>
+                      {/* {imageList.map((image, index) => (
+                        <img src={image['data_url']} alt="" key={index} class='w-full' />
+                      ))} */}
+                      <img src={selectedFile} alt="" class='w-full' />
+                      <button onClick={() => onImageUpdate(0)} class='mt-7 px-3 py-2 rounded bg-green-500 text-white'>
+                        Reselect An Image
+                      </button>
+                    </div>
+                  </>
+                )}
+              </div>
+            )}
+          </ReactImageUploading>
+        </div>
+        <div class='w-6/12 flex flex-col h-full justify-center items-center px-4'>
+          {/* <img src='https://i.ytimg.com/vi/1_q8txKyg4E/maxresdefault.jpg' alt='' class='w-full' />
+          <h1 class='mt-7 px-3 py-2 rounded'>Filtered</h1> */}
+        </div>
+      </div>
     </div>
   );
 }
